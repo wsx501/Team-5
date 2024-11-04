4dfdee0b30b7706b6c795627b8b18e8ec51fd076
ggx500 <ggx500@york.ac.uk>
2024-11-04 23:21:13 +0000

    added a button, tried to implement button clicking

--- core/src/main/java/com/unisim/game/main.java
+++ core/src/main/java/com/unisim/game/main.java
@@ -2,6 +2,8 @@ package com.unisim.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
@@ -17,7 +19,7 @@ import com.badlogic.gdx.utils.Timer;
 import java.util.TimerTask;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class main extends ApplicationAdapter {
+public class main extends ApplicationAdapter implements InputProcessor {
     //private SpriteBatch batch;
     //private Texture image;
 
@@ -32,7 +34,7 @@ public class main extends ApplicationAdapter {
     TextButton.TextButtonStyle playButtonStyle;
     BitmapFont font;
     Skin skin;
-    TextureAtlas atlas;
+    TextureAtlas buttonAtlas;
 
     @Override
     public void create() {
@@ -49,6 +51,12 @@ public class main extends ApplicationAdapter {
         camera.update();
 
         menuStage = new Stage();
+        button playButton = new button("buttons/Play Button.png",
+                                (Gdx.graphics.getHeight()/2f)+400f,
+                                Gdx.graphics.getWidth()/2f,
+                                0.5f);
+        menuStage.addActor(playButton);
+
 
         mainStage = new Stage();
         mainStage.addActor(new gameMap());
@@ -76,14 +84,11 @@ public class main extends ApplicationAdapter {
         switch(sceneId){
             case 0:
                 Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+                Gdx.input.setInputProcessor(menuStage);
+                //if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+                  //  if (());
+                //}
                 menuStage.draw();
-
-                Timer.schedule(new Timer.Task(){
-                    @Override
-                    public void run() {
-                        main.this.sceneId = 1;
-                    }
-                }, 5f);
                 break;
             case 1:
                 Gdx.gl.glClearColor(0f, 0.23f, 0.17f, 1);
@@ -94,9 +99,62 @@ public class main extends ApplicationAdapter {
 
 
 
+
+
     @Override
     public void dispose() {
         //batch.dispose();
         //image.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char c) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        Vector2 coord = menuStage.screenToStageCoordinates(new Vector2((float) screenX, (float) screenY));
+        Actor hitActor = menuStage.hit(coord.x, coord.y, false);
+
+        if (hitActor != null) {
+            Gdx.app.exit();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchCancelled(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int i, int i1, int i2) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float v, float v1) {
+        return false;
     }
 }
